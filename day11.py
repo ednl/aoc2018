3a0b6e3d9fd5999fbecfb58416a9e9dc60cd767f
Ewoud Dronkert <ewoud@dronkert.net>
2021-02-11 15:00:57 +0100

    day 11

--- day11.py
+++ day11.py
@@ -1,34 +1,36 @@
 import numpy as np
-# from scipy.
+from scipy.signal import convolve2d
 
 def power(x, y, s):
     return ((x + 10) * y + s) * (x + 10) // 100 % 10 - 5
 
-# print(power(  3,   5,  8))
-# print(power(122,  79, 57))
-# print(power(217, 196, 39))
-# print(power(101, 153, 71))
-
-dim = 300
-grid = np.zeros((dim, dim), dtype=np.int8)
-
-# serial = 18
-# for y in range(dim):
-#     for x in range(dim):
-#         grid[y, x] = power(x + 1, y + 1, serial)
-
-# print(grid[43:48, 31:36])
-
-# serial = 42
-# for y in range(dim):
-#     for x in range(dim):
-#         grid[y, x] = power(x + 1, y + 1, serial)
-
-# print(grid[59:64, 19:24])
+dim1 = 300
+grid = np.zeros((dim1, dim1), dtype=np.int64)
 
 serial = 2866
-for y in range(dim):
-    for x in range(dim):
+for y in range(dim1):
+    for x in range(dim1):
+        # The puzzle uses 1-based indexing
+        # Also for debug printing: y=rows, x=cols
         grid[y, x] = power(x + 1, y + 1, serial)
 
-kernel = np.ones((3, 3), dtype=np.int8)
+dim2 = 3  # start with 3x3 because that's part 1
+curmax = absmax = 0
+
+# Use clever condition for positive maximum
+# because the trend is negative for larger squares (power is -5..+4)
+while curmax >= 0 and dim2 <= dim1:
+    # Convolution kernel all ones => sum of all the squares
+    kernel = np.ones((dim2, dim2), dtype=np.int64)
+    # mode='valid' means do not extend beyond dim1,
+    # so the result of the convolution will be smaller by half dim2
+    squares = convolve2d(grid, kernel, mode='valid')
+    # Get the index of the maximum value
+    my, mx = np.unravel_index(squares.argmax(), squares.shape)
+    # The maximum value itself for this dim2
+    curmax = squares[my, mx]
+    if curmax > absmax:
+        absmax = curmax
+        # Plus 1 because puzzle uses 1-based indexing
+        print("%d,%d,%d" % (mx + 1, my + 1, dim2))
+    dim2 += 1
